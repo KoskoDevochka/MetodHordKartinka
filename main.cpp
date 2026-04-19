12615d505ce5d1c2eab39ac95409fe61a2763536
Suonmur <varya.khaynitskaya@mail.ru>
2026-04-20 02:56:18 +1000

    Update main.cpp
    Сервер подключение бд

--- main.cpp
+++ main.cpp
@@ -4,9 +4,14 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    // Инициализация БД (синглтон создастся автоматически)
+    Database* db = Database::getInstance();
+    Q_UNUSED(db); // чтобы не было warning о неиспользуемой переменной
     
     // Правильное использование синглтона - через getInstance()
     MyTcpServer* server = MyTcpServer::getInstance();
+    Q_UNUSED(server);
     
     // Если нужно использовать Database:
     // Database* db = Database::getInstance();
